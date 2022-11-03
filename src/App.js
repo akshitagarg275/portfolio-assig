10a250325aaf0075ea6011ce993a73907390ebbf
akshitagarg275 <akshitagarg275@gmail.com>
2022-11-03 11:01:57 +0530

    final proj

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+import NavBar from './Nav';
+import Header from './Header';
+import PortfolioSection from './PortfolioSection';
+import About from './About';
+import Contact from './Contact'
+import Footer from './Footer'
+import CopyRight from './Copyright';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar/>
+      <Header/>
+      <PortfolioSection/>
+      <About/>
+      <Contact/>
+      <Footer/>
+      <CopyRight/>
+    </>
   );
 }
 
